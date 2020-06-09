1864964ae29398cfd4b0c5e3dff4d732c77d2ce7
Han Nguyen <h.bnguyen2001@gmail.com>
2020-06-09 04:28:18 -0400

    Flix app

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by nguyen thy on 6/9/20.
+//  Copyright © 2020 Han Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
